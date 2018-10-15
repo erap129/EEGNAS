0e6f6c2f15a9a0b1c9aae225a0ad70d572157b14
eladr <eladr@post.bgu.ac.il>
2018-10-15 09:45:55 +0300

    added add_filters function - doesn't work

--- naiveNAS.py
+++ naiveNAS.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential, Model
 from keras.layers import Conv2D, Dense, Flatten, Activation, MaxPool2D, Lambda, Dropout
 from keras.utils import to_categorical
+from keras.callbacks import EarlyStopping
 
 import random
 
@@ -21,12 +22,12 @@ class NaiveNAS:
     def find_best_model(self):
         base_model = self.base_model()
         model = self.add_conv_maxpool_block(base_model)
+        model = self.add_filters(model)
         model = self.finalize_model(model)
         earlystopping = EarlyStopping(monitor='val_acc', min_delta=0, patience=10)
-        model.fit(X_train, y_train, epochs=50, validation_data=(X_valid, y_valid),
+        model.fit(self.X_train, self.y_train, epochs=50, validation_data=(self.X_valid, self.y_valid),
                   callbacks=[earlystopping])
-        y_test = to_categorical(self.y_test, num_classes=self.n_classes)
-        res = model.evaluate(self.X_test, y_test)[1] * 100
+        res = model.evaluate(self.X_test, self.y_test) * 100
         print('model accuracy:', res[1] * 100)
 
     def base_model(self, n_filters_time=25, n_filters_spat=25, filter_time_length=10):
@@ -41,7 +42,7 @@ class NaiveNAS:
 
         # Also, they added dropout for each input the conv layers except the first! I dropped out only in the end
 
-        model.add(Conv2D(name='spatial_filter', filters=n_filters_spat, kernel_size=(self.n_chans, 1), strides=(1,1), activation='elu'))
+        model.add(Conv2D(name='spatial_convolution', filters=n_filters_spat, kernel_size=(self.n_chans, 1), strides=(1,1), activation='elu'))
         model.add(MaxPool2D(pool_size=(1,3), strides=(1,3)))
         return model
 
@@ -61,9 +62,27 @@ class NaiveNAS:
 
         output = model.layers[-1].output
         conv_layer = Conv2D(filters=conv_filter_num, kernel_size=(1, conv_width),
-                            strides=(1, 1), activation='elu')(output)
-        maxpool_layer = MaxPool2D(pool_size=(1, maxpool_len), strides=(1, 3))
+                            strides=(1, 1), activation='elu', name='convolution')(output)
+        maxpool_layer = MaxPool2D(pool_size=(1, maxpool_len), strides=(1, 3))(conv_layer)
         model = Model(input=model.layers[0].input, output=maxpool_layer)
         return model
+
+    # def add_skip_connection(self, model):
+    #     conv_indices = [i for i, layer in enumerate(model.layers) if layer.get_config()['class_name']=='Conv2D']
+    #     if len(conv_indices) < 2:
+    #         return
+    #     else:
+
+    def add_filters(self, model):
+        conv_indices = [i for i, layer in enumerate(model.layers) if 'convolution' in layer.get_config()['name']]
+        random_conv_index = random.randint(0, len(conv_indices)-1)
+        factor = 2
+        conv_layer = model.layers[random_conv_index]
+        conv_layer.filters = conv_layer.filters * factor
+        print('new conv layer filters after transform is:', conv_layer.filters)
+        print('just to make sure, its:', model.layers[random_conv_index].filters)
+        return model
+
+
 
 
