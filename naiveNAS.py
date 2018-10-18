74164aa50da367366078c91a331f16cf476cb47d
eladr <eladr@post.bgu.ac.il>
2018-10-18 15:19:32 +0300

    created DAG structure for layers. Still not working.

--- naiveNAS.py
+++ naiveNAS.py
@@ -5,16 +5,27 @@ import copy
 from simanneal import Annealer
 import numpy as np
 import time
+from keras.utils import plot_model
 from keras.callbacks import EarlyStopping
-
+from toposort import toposort_flatten
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 import random
 
 
 class Layer:
     running_id = 0
+
     def __init__(self):
         self.id = Layer.running_id
+        self.connections = []
+        self.parent = None
+        self.keras_layer = None
         Layer.running_id += 1
+
+    def make_connection(self, other):
+        self.connections.append(other)
+        other.parent = self
 
 
 class InputLayer(Layer):
@@ -70,41 +81,58 @@ def print_structure(structure):
         print('layer type:', layer.__class__.__name__, 'layer id:', layer.id)
     print('-----------------------------------------------')
 
+
+def create_topo_layers(layers):
+    layer_dict = {}
+    for layer in layers:
+        layer_dict[layer] = {x for x in layer.connections}
+    return list(reversed(toposort_flatten(layer_dict)))
+
+
 class MyModel(Model):
-    def __init__(self, structure=[], *args, **kwargs):
+    def __init__(self, layer_collection={}, *args, **kwargs):
         super(MyModel, self).__init__(*args, **kwargs)
-        self.structure = structure
+        self.layer_collection = layer_collection
 
-    def new_model_from_structure(self, structure):
-        model_layers = []
-
-        for layer in structure:
+    @staticmethod
+    def new_model_from_structure(layer_collection):
+        topo_layers = create_topo_layers(layer_collection.values())
+        for layer in topo_layers:
             if isinstance(layer, InputLayer):
-                keras_layer = Input(shape=(layer.shape_height, layer.shape_width, 1))
-                x = keras_layer
+                keras_layer = Input(shape=(layer.shape_height, layer.shape_width, 1), name=str(layer.id))
+                layer.keras_layer = keras_layer
             elif isinstance(layer, MaxPoolLayer):
-                keras_layer = MaxPool2D(pool_size=(1, layer.pool_width), strides=(1, layer.stride_width))
-                x = keras_layer(x)
+                keras_layer = MaxPool2D(pool_size=(1, layer.pool_width), strides=(1, layer.stride_width),
+                                        name=str(layer.id))
+                layer.keras_layer = keras_layer
             elif isinstance(layer, ConvLayer):
                 keras_layer = Conv2D(filters=layer.filter_num, kernel_size=(layer.kernel_height, layer.kernel_width),
-                           strides=(1, 1), activation='elu')
-                x = keras_layer(x)
+                                     strides=(1, 1), activation='elu', name=str(layer.id))
+                layer.keras_layer = keras_layer
             elif isinstance(layer, CroppingLayer):
                 keras_layer = Cropping2D(((layer.height_crop_top, layer.height_crop_bottom),
-                                (layer.width_crop_left, layer.width_crop_right)))
-                x = keras_layer(x)
+                                          (layer.width_crop_left, layer.width_crop_right)), name=str(layer.id))
+                layer.keras_layer = keras_layer
             elif isinstance(layer, ZeroPadLayer):
                 keras_layer = ZeroPadding2D(((layer.height_pad_top, layer.height_pad_bottom),
-                                   (layer.width_pad_left, layer.width_pad_right)))
-                x = keras_layer(x)
+                                             (layer.width_pad_left, layer.width_pad_right)), name=str(layer.id))
+                layer.keras_layer = keras_layer
             elif isinstance(layer, ConcatLayer):
-                print_structure(structure)
                 print('about to concatenate layers:', layer.first_layer_index, 'and:', layer.second_layer_index)
-                keras_layer = Concatenate()([next((x for x, id in model_layers if id == layer.first_layer_index), None),
-                                             next((x for x, id in model_layers if id == layer.second_layer_index), None)])
-                x = keras_layer
-            model_layers.append((x, layer.id))
-        return MyModel(structure=structure, inputs=model_layers[0][0], output=x)
+                keras_layer = Concatenate(name=str(layer.id))(
+                    [layer_collection[layer.first_layer_index].keras_layer,
+                     layer_collection[layer.second_layer_index].keras_layer])
+                layer.keras_layer = keras_layer
+            if layer.parent is not None:
+                try:
+                    layer.keras_layer = layer.keras_layer(layer.parent.keras_layer)
+                except TypeError:
+                    print(layer.keras_layer.__class__.__name__)
+                except ValueError:
+                    print(layer.keras_layer.__class__.__name__)
+
+        return MyModel(layer_collection=layer_collection,
+                       inputs=layer_collection[0].keras_layer, output=layer.keras_layer)
 
 
 class NaiveNAS:
@@ -135,7 +163,7 @@ class NaiveNAS:
             op_index = random.randint(0, len(operations) - 1)
             num_of_ops += 1
             # model = operations[op_index](curr_model, num_of_ops)
-            model = self.add_skip_connection_concat(curr_model, num_of_ops)
+            model = self.add_skip_connection_concat(curr_model)
             final_model = self.finalize_model(model)
             final_model.fit(self.X_train, self.y_train, epochs=50, validation_data=(self.X_valid, self.y_valid),
                       callbacks=[earlystopping])
@@ -157,39 +185,49 @@ class NaiveNAS:
         print('model accuracy:', res[1] * 100)
 
     def base_model(self, n_filters_time=25, n_filters_spat=25, filter_time_length=10):
-        inputs = Input(shape=(self.n_chans, self.input_time_len, 1))
-        temporal_conv = Conv2D(name='temporal_convolution', filters=n_filters_time, input_shape=(self.n_chans, self.input_time_len, 1),
-                         kernel_size=(1, filter_time_length), strides=(1,1), activation='elu')(inputs)
+        # inputs = Input(shape=(self.n_chans, self.input_time_len, 1))
+        # temporal_conv = Conv2D(name='temporal_convolution', filters=n_filters_time, input_shape=(self.n_chans, self.input_time_len, 1),
+        #                  kernel_size=(1, filter_time_length), strides=(1,1), activation='elu')(inputs)
+        #
+        # # note that this is a different implementation from the paper!
+        # # they didn't put an activation function between the first two convolutions
+        #
+        # # Also, in the paper they implemented batch-norm before each non-linearity - which I didn't do!
+        #
+        # # Also, they added dropout for each input the conv layers except the first! I dropped out only in the end
+        #
+        # spatial_conv = Conv2D(name='spatial_convolution', filters=n_filters_spat,
+        #                       kernel_size=(self.n_chans, 1), strides=(1,1), activation='elu')(temporal_conv)
+        # maxpool = MaxPool2D(pool_size=(1, 3), strides=(1, 3))(spatial_conv)
+        # model = MyModel(inputs=inputs, outputs=maxpool)
+        # model.structure.clear()
+        layer_collection = {}
+        inputs = InputLayer(shape_height=self.n_chans, shape_width=self.input_time_len)
+        layer_collection[inputs.id] = inputs
+        conv_time = ConvLayer(kernel_width=filter_time_length, kernel_height=1, filter_num=n_filters_time)
+        layer_collection[conv_time.id] = conv_time
+        inputs.make_connection(conv_time)
+        conv_spat = ConvLayer(kernel_width=1, kernel_height=self.n_chans, filter_num=n_filters_spat)
+        layer_collection[conv_spat.id] = conv_spat
+        conv_time.make_connection(conv_spat)
+        maxpool = MaxPoolLayer(pool_width=3, stride_width=3)
+        layer_collection[maxpool.id] = maxpool
+        conv_spat.make_connection(maxpool)
 
-        # note that this is a different implementation from the paper!
-        # they didn't put an activation function between the first two convolutions
-
-        # Also, in the paper they implemented batch-norm before each non-linearity - which I didn't do!
-
-        # Also, they added dropout for each input the conv layers except the first! I dropped out only in the end
-
-        spatial_conv = Conv2D(name='spatial_convolution', filters=n_filters_spat,
-                              kernel_size=(self.n_chans, 1), strides=(1,1), activation='elu')(temporal_conv)
-        maxpool = MaxPool2D(pool_size=(1, 3), strides=(1, 3))(spatial_conv)
-        model = MyModel(inputs=inputs, outputs=maxpool)
-        model.structure.clear()
-        model.structure.append(InputLayer(shape_height=self.n_chans, shape_width=self.input_time_len))
-        model.structure.append(ConvLayer(kernel_width=filter_time_length, kernel_height=1, filter_num=n_filters_time))
-        model.structure.append(ConvLayer(kernel_width=1, kernel_height=self.n_chans, filter_num=n_filters_spat))
-        model.structure.append(MaxPoolLayer(pool_width=3, stride_width=3))
-        # model.structure.append('input-'+str(self.n_chans)+'-'+str(self.input_time_len))
-        # model.structure.append('convolution-'+str(n_filters_time)+'-1-'+str(filter_time_length))
-        # model.structure.append('convolution-'+str(n_filters_spat)+'-'+str(self.n_chans)+'-1')
-        # model.structure.append('maxpool-3-3')
-        return model
+        # model.structure.append(InputLayer(shape_height=self.n_chans, shape_width=self.input_time_len))
+        # model.structure.append(ConvLayer(kernel_width=filter_time_length, kernel_height=1, filter_num=n_filters_time))
+        # model.structure.append(ConvLayer(kernel_width=1, kernel_height=self.n_chans, filter_num=n_filters_spat))
+        # model.structure.append(MaxPoolLayer(pool_width=3, stride_width=3))
+        return MyModel.new_model_from_structure(layer_collection)
 
     def finalize_model(self, model):
         output = model.layers[-1].output
         flatten_layer = Flatten()(output)
         prediction_layer = Dense(self.n_classes, activation='softmax', name='prediction_layer')(flatten_layer)
-        model = MyModel(structure=model.structure, input=model.layers[0].input, output=prediction_layer)
+        model = MyModel(layer_collection=model.layer_collection, input=model.layers[0].input, output=prediction_layer)
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
         model.summary()
+        plot_model(model, to_file='model.png')
         return model
 
     def add_conv_maxpool_block(self, model, num_of_ops):
@@ -216,23 +254,23 @@ class NaiveNAS:
         # model.structure.append('maxpool-'+str(maxpool_len)+'-'+str(maxpool_stride))
         return model
 
-    def add_skip_connection_concat(self, model, num_of_ops):
-        to_concat = random.sample(range(len(model.structure)), 2)
+    def add_skip_connection_concat(self, model):
+        to_concat = random.sample(range(Layer.running_id), 2)  # choose 2 random layer id's
         first_layer_index = np.min(to_concat)
         second_layer_index = np.max(to_concat)
-        first_layer = model.layers[first_layer_index]
-        second_layer = model.layers[second_layer_index]
-        first_shape = first_layer.output_shape
-        second_shape = second_layer.output_shape
+        first_layer = model.get_layer(str(first_layer_index))
+        second_layer = model.get_layer(str(second_layer_index))
+        first_shape = model.get_layer(str(first_layer_index)).output.shape
+        second_shape = model.get_layer(str(second_layer_index)).output.shape
         print('first layer shape is:', first_shape)
         print('second layer shape is:', second_shape)
-        print('first layer index for concatLayer is:', first_layer_index, 'second layer index is:', second_layer_index)
-        print('-----------model summary in add skip connection------------')
-        model.summary()
-        print('-----------model structure in add skip connection----------')
-        print_structure(model.structure)
-        height_diff = second_shape[1] - first_shape[1]
-        width_diff = second_shape[2] - first_shape[2]
+        # print('first layer index for concatLayer is:', first_layer_index, 'second layer index is:', second_layer_index)
+        # print('-----------model summary in add skip connection------------')
+        # model.summary()
+        # print('-----------model structure in add skip connection----------')
+        # print_structure(model.structure)
+        height_diff = int(first_shape[1]) - int(second_shape[1])
+        width_diff = int(first_shape[2]) - int(second_shape[2])
         height_crop_top = height_crop_bottom = np.abs(int(height_diff / 2))
         width_crop_left = width_crop_right = np.abs(int(width_diff / 2))
         if height_diff % 2 == 1:
@@ -247,22 +285,37 @@ class NaiveNAS:
             ChosenWidthClass = ZeroPadLayer
         else:
             ChosenWidthClass = CroppingLayer
-        layerdiff = 0
+        # layerdiff = 0
+        first_layer = model.layer_collection[first_layer_index]
+        second_layer = model.layer_collection[second_layer_index]
+        next_layer = first_layer
         if height_diff != 0:
-            layerdiff += 1
+            # layerdiff += 1
             heightChanger = ChosenHeightClass(height_crop_top, height_crop_bottom, 0, 0)
-            model.structure.insert(second_layer_index + layerdiff, heightChanger)
+            model.layer_collection[heightChanger.id] = heightChanger
+            first_layer.make_connection(heightChanger)
+            next_layer = heightChanger
+            # model.structure.insert(second_layer_index + layerdiff, heightChanger)
         if width_diff != 0:
-            layerdiff += 1
+            # layerdiff += 1
             widthChanger = ChosenWidthClass(0, 0, width_crop_left, width_crop_right)
-            model.structure.insert(second_layer_index + layerdiff, widthChanger)
-        model.structure.insert(second_layer_index + layerdiff + 1, ConcatLayer(
-            model.structure[first_layer_index].id, model.structure[second_layer_index + layerdiff].id))
-        # model.structure.insert(second_layer_index + 1, 'cropping-' + str(height_diff / 2) + '-' +
-        #                        str(width_diff / 2))
-        # model.structure.insert(second_layer_index+2, 'concatenate-'+str(first_layer_index)+'-'+
-        #                         str(second_layer_index+1))
-        return model.new_model_from_structure(copy.deepcopy(model.structure))
+            model.layer_collection[widthChanger.id] = widthChanger
+            next_layer.make_connection(widthChanger)
+            next_layer = widthChanger
+            # model.structure.insert(second_layer_index + layerdiff, widthChanger)
+        concat = ConcatLayer(next_layer.id, second_layer_index)
+        model.layer_collection[concat.id] = concat
+        next_layer.connections.append(concat)
+        concat.connections = second_layer.connections
+        for lay in concat.connections:
+            if not isinstance(lay, ConcatLayer):
+                lay.parent = concat
+        second_layer.connections = []
+        second_layer.connections.append(concat)
+
+        # model.structure.insert(second_layer_index + layerdiff + 1, ConcatLayer(
+        #     model.structure[first_layer_index].id, model.structure[second_layer_index + layerdiff].id))
+        return model.new_model_from_structure(model.layer_collection)
 
 
     def add_filters(self, model):
@@ -276,5 +329,6 @@ class NaiveNAS:
         # convolution, depth, height, width = model.structure[conv_indices[random_conv_index]].split('-')
         # model.structure[conv_indices[random_conv_index]] = convolution+'-'+str(int(depth)*factor)+'-'+height+'-'+width
         model.structure[conv_indices[random_conv_index]].filter_num *= factor
-        return model.new_model_from_structure(copy.deepcopy(model.structure), self)
+        return model.new_model_from_structure(copy.deepcopy(model.structure),
+                                              copy.deepcopy(model.layer_collection), self)
 
