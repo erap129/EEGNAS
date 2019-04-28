f821b669fa6e16e3f1d83f4454beede8effea941
user <eladr@post.bgu.ac.il>
2019-04-28 18:09:40 +0300

    worked on layer visualization

--- visualiazation/src/cnn_layer_visualization.py
+++ visualiazation/src/cnn_layer_visualization.py
@@ -11,10 +11,13 @@ from braindecode.torch_ext.util import np_to_var
 from visualiazation.src.misc_functions import preprocess_image, recreate_image, save_image, create_mne
 import matplotlib.pyplot as plt
 from models_generation import target_model
+from naiveNAS import NaiveNAS
 import globals
 import numpy as np
-from data_preprocessing import get_ner_train_val_test, get_bci_iv_2a_train_val_test
+from data_preprocessing import get_ner_train_val_test, get_bci_iv_2a_train_val_test, get_bci_iv_2b_train_val_test
 from scipy import signal
+from scipy.io import savemat
+from BCI_IV_2a_experiment import get_normal_settings, set_params_by_dataset
 
 class CNNLayerVisualization():
     """
@@ -38,44 +41,19 @@ class CNNLayerVisualization():
         # Hook the selected layer
         self.model[self.selected_layer].register_forward_hook(hook_function)
 
-    @staticmethod
-    def create_spectrogram_2(data, fs):
-        plt.subplot(211)
-        plt.plot(np.arange(len(data)), data)
-        plt.xlabel('Sample')
-        plt.ylabel('Amplitude')
-        plt.subplot(212)
-        frequencies, times, spectrogram = signal.spectrogram(data, fs=fs, window='hamming', noverlap=150)
-        plt.pcolormesh(times, frequencies, 10*np.log10(spectrogram))
-        plt.ylim((0,50))
-        plt.ylabel('Frequency [Hz]')
-        plt.xlabel('Time [sec]')
-        plt.show()
-
-    @staticmethod
-    def create_spectrogram(data):
-        plt.subplot(211)
-        plt.plot(np.arange(len(data)), data)
-        plt.xlabel('Sample')
-        plt.ylabel('Amplitude')
-        # Plot the spectrogram
-        plt.subplot(212)
-        powerSpectrum, freqenciesFound, time, imageAxis = plt.specgram(data, NFFT=100, Fs=250, noverlap=25)
-        plt.xlabel('Time')
-        plt.ylabel('Frequency')
-        plt.show()
-
     def visualise_layer_with_hooks(self):
         # Hook the selected layer
         self.hook_layer()
         # Generate a random image
-        random_image = np.float32(np.random.uniform(-1, 1, (1, 22, 1125, 1)))
+        random_image = np.float32(np.random.uniform(-1, 1, (1, globals.get('eeg_chans'),
+                                                            globals.get('input_time_len'), 1)))
         # Process image and return variable
         # processed_image = preprocess_image(random_image, False)
-        processed_image = Variable(np_to_var(random_image), requires_grad=True)
+        processed_image = torch.tensor(np_to_var(random_image), requires_grad=True, device='cuda')
+        # processed_image = torch.Tensor.new_tensor(data=torch.Tensor(random_image), requires_grad=True, device='cuda')
         # Define optimizer for the image
         optimizer = Adam([processed_image], lr=0.1, weight_decay=1e-6)
-        for i in range(1, 101):
+        for i in range(1, 1001):
             optimizer.zero_grad()
             # Assign create image to a variable to move forward in the model
             x = processed_image
@@ -91,22 +69,22 @@ class CNNLayerVisualization():
             # Loss function is the mean of the output of the selected layer/filter
             # We try to minimize the mean of the output of that specific filter
             loss = -torch.mean(self.conv_output)
-            print('Iteration:', str(i), 'Loss:', "{0:.2f}".format(loss.data.numpy()))
+            print('Iteration:', str(i), 'Loss:', "{0:.2f}".format(loss.data.cpu().numpy()))
             # Backward
             loss.backward()
             # Update image
             optimizer.step()
             # Recreate image
-            self.created_image = create_mne(processed_image)
+            # self.created_image = create_mne(processed_image)
 
-            if i % 10 == 0:
-                CNNLayerVisualization.create_spectrogram_2(np.array(processed_image.detach().numpy().squeeze()[0]), 250)
+            if i % 100 == 0:
+                savemat(f'X_step_{int(i/10)}.mat', {'X': processed_image.detach().cpu().numpy().squeeze()[None, :, :]})
 
 
 def plot_real_EEG():
-    globals.set('valid_set_fraction', 0.2)
-    train, val, test = get_bci_iv_2a_train_val_test('../../data/BCI_IV', 4, 0)
+    # train, val, test = get_bci_iv_2a_train_val_test('../../data/BCI_IV', 4, 0)
     # train, val, test = get_ner_train_val_test('../../data/')
+    train, val, test = get_bci_iv_2b_train_val_test(1)
     data = train.X[0].squeeze()
     for channel in data:
         CNNLayerVisualization.create_spectrogram_2(channel, fs=250)
@@ -117,12 +95,29 @@ if __name__ == '__main__':
     globals.set('input_time_len', 1125)
     globals.set('n_classes', 4)
     globals.set('eeg_chans', 22)
+    globals.set('valid_set_fraction', 0.2)
+    globals.set('dataset', 'BCI_IV_2b')
+    globals.set('batch_size', 60)
+    globals.set('do_early_stop', True)
+    globals.set('remember_best', True)
+    globals.set('max_epochs', 50)
+    globals.set('max_increase_epochs', 3)
+    globals.set('final_max_epochs', 800)
+    globals.set('final_max_increase_epochs', 80)
+    globals.set('cuda', True)
+    set_params_by_dataset()
     selection = 'deep4'
     cnn_layer = {'evolution': 6, 'deep4': 20}
     filter_pos = {'evolution': 5, 'deep4': 150}
-    pretrained_model = {'evolution': torch.load('../models/best_model_9_8_6_7_2_1_3_4_5.th'),
+    model = {'evolution': torch.load('../models/best_model_9_8_6_7_2_1_3_4_5.th'),
                         'deep4': target_model('deep')}
-    # layer_vis = CNNLayerVisualization(pretrained_model[selection], cnn_layer[selection], filter_pos[selection])
-    # layer_vis.visualise_layer_with_hooks()
-    #
-    plot_real_EEG()
+    train, val, test = get_bci_iv_2b_train_val_test(subject_id=1)
+    stop_criterion, iterator, loss_function, monitors = get_normal_settings()
+    naiveNAS = NaiveNAS(iterator=iterator, exp_folder=None, exp_name=None,
+                        train_set=train, val_set=val, test_set=test,
+                        stop_criterion=stop_criterion, monitors=monitors, loss_function=loss_function,
+                        config=globals.config, subject_id='all', fieldnames=None, strategy='cross_subject',
+                        evolution_file=None, csv_file=None)
+    _, _, pretrained_model, _, _ = naiveNAS.evaluate_model(model[selection], final_evaluation=True)
+    layer_vis = CNNLayerVisualization(pretrained_model, cnn_layer[selection], filter_pos[selection])
+    layer_vis.visualise_layer_with_hooks()
